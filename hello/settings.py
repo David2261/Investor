cb43b8ae039767cb9b5b97e73ae2bc884616c49c
David2261 <bulatnasirov2003@gmail.com>
2021-09-05 10:56:45 +0300

    add static and links for img

--- hello/settings.py
+++ hello/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
