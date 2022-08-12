632228a23e57eadba323166051b29733fa06817c
David2261 <bulatnasirov2003@gmail.com>
2022-08-12 16:49:14 +0300

    add navbar function without design

--- hello/apps/frontend/src/App.js
+++ hello/apps/frontend/src/App.js
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading"
-    };
-  }
-
-  componentDidMount() {
-    fetch("api/lead")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
-  }
-
   render() {
     return (
-      <ul>
-        {this.state.data.map(contact => {
-          return (
-            <li key={contact.id}>
-              {contact.name} - {contact.email}
-            </li>
-          );
-        })}
-        <h1>
-        Hello React
-        </h1>
-      </ul>
+      <>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+      </Router>
+      </>
     );
   }
 }
