57d0988deff240d495916a30a0a20b6bb9aa4f02
David2261 <bulatnasirov2003@gmail.com>
2022-10-14 23:14:48 +0300

    Create base for feature update of cbv in view and urls files

--- hello/apps/articles/urls.py
+++ hello/apps/articles/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     # path('', views.index, name="main-view" ),
+    path('', HomePage.as_view(), name="home_page"),
+    path('about/', AboutPage.as_view(), name="about_page"),
 ]
