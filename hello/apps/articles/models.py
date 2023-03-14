cac26a48d132da523571a6f508292bd40e2e2491
David2261 <bulatnasirov2003@gmail.com>
2023-03-14 21:41:52 +0300

    Add technical task on rus/eng lang

--- hello/apps/articles/models.py
+++ hello/apps/articles/models.py
@@ -5,26 +5,18 @@ from django.urls import reverse
 from authentication.models import User
 
 from django.conf import settings
+from .regular_models import BasePost
+
 
 logging.config.dictConfig(settings.LOGGING)
 logger = logging.getLogger("dev")
 log_info = logging.getLogger("root")
 
+
 STATUS_CHOICES = (
    ('draft', 'Draft'),
    ('published', 'Published'),
 )
-
-
-class BasePost(models.Model):
-	title = models.CharField(verbose_name = "Заголовок статьи", help_text = "Название статьи", max_length = 255, null = False, blank = False, primary_key = True)
-	time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-
-	class Meta:
-		abstract = True
-
-	def __str__(self):
-		return self.title
 
 
 class Ip(models.Model):
