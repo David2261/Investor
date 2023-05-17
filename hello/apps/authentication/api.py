6e47347c01c3f03bb11c9787300107c78f239b5b
David2261 <bulatnasirov2003@gmail.com>
2023-05-17 13:27:51 +0300

    Lintning all files with flake8

--- hello/apps/authentication/api.py
+++ hello/apps/authentication/api.py
@@ -1,12 +1,9 @@
 from rest_framework import viewsets, permissions
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group
 
 from .models import User
 from .serializers import UserSerializer
 from .serializers import GroupSerializer
-from .serializers import LoginSerializer
-from .serializers import RegistrationSerializer
-
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,16 +15,8 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = Group.objects.all()
     permissions_classes = [
         permissions.AllowAny
     ]
-    serializer_class = UserSerializer
-
-
-class LoginViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    permissions_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = LoginSerializer
+    serializer_class = GroupSerializer
