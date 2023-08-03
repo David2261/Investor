932e97164a1b9614f21b16a0cfd1e690f01e0b0e
David2261 <bulatnasirov2003@gmail.com>
2023-08-04 00:25:28 +0300

    Add template for tickers from stock market

--- frontend/src/components/StockMarket/LiveStock.tsx
+++ frontend/src/components/StockMarket/LiveStock.tsx
@@ -2,51 +2,54 @@ import { useState } from 'react';
 import api from '/src/api/AVStockAPI.tsx';
 
 
+interface IndexMarketProps {
+	ticker: string;
+	percent: string; 
+}
+
+const IndexMarket: React.FC = ({ticker, percent}): IndexMarketProps => {
+	return <div className="px-4 bg-white text-black hover:text-white hover:bg-zinc-950 rounded-md py-2">
+		<p className=" font-bold">{ticker} {percent}%</p>
+	</div>
+};
+
 export default function LiveStock() {
 	let [responseData, setResponseData] = useState('');
 	let [ticker, setTicker] = useState('');
-    let [message, setMessage] = useState('');
+	let [message, setMessage] = useState('');
 
 	const fetchData = (e) => {
-        e.preventDefault()
+		e.preventDefault()
 
-        setMessage('Loading...')
+		setMessage('Loading...')
 
-        api.stockMarket(ticker)
-        .then((response)=>{
-            setResponseData(response.data)
-            setMessage('')
-            console.log(response)
-        })
-        .catch((error) => {
-            setMessage('Error')
-            console.log(error)
-        })
-    }
+		api.stockMarket(ticker)
+		.then((response)=>{
+			setResponseData(response.data)
+			setMessage('')
+			console.log(response)
+		})
+		.catch((error) => {
+			setMessage('Error')
+			console.log(error)
+		})
+	}
 
 	return (
-		<div className="flex w-full">
-			<form onSubmit={fetchData}>
-                <fieldset>
-                    <legend>Search Stock Market</legend>
-                    <label htmlFor="ticker">Enter stock ticker
-                        <input
-                            required
-                            name="ticker"
-                            id="ticker"
-                            type='text'
-                            placeholder='SPY'
-                            value={ticker}
-                            onChange={(e) => setTicker(e.target.value)}
-                        />
-                    </label>
-                    <button type='submit'>Submit</button>
-                </fieldset>
-            </form>
-            <p>{message}</p>
-            <h3>Symbol: {responseData ? responseData.symbol : ''}</h3>
-            <p>Daily Time Series with Splits and Dividend Events</p>
-            <small>Last Refresh: {responseData ? responseData.refreshed : ''}</small>
+		<div className="hiden md:flex w-full ml-4 py-4 items-center border-b-2 border-stone-200">
+			<div className="flex flex-col justify-between items-center w-auto">
+				<div><p className="uppercase text-gray-400 font-bold">us markets open in:</p></div>
+				<div><p className="text-gray-400 font-bold">TIMER</p></div>
+				<div><p className="text-gray-600 font-light">In the news</p></div>
+			</div>
+			<div className="flex flex-row">
+				<IndexMarket ticker="Dow Jones" percent="2.23" />
+				<IndexMarket ticker="Nasdaq" percent="0.12" />
+				<IndexMarket ticker="S&P 500" percent="0.43" />
+				<IndexMarket ticker="META" percent="0.73" />
+				<IndexMarket ticker="TSLA" percent="1.1" />
+				<IndexMarket ticker="BABA" percent="1.5" />
+			</div>
 		</div>
 	)
 }
