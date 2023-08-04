789bb2ff77d1dd5924e0a9af4e0baba21d4578d3
David2261 <bulatnasirov2003@gmail.com>
2023-08-04 23:54:02 +0300

    Comleted template for stock tickers

--- frontend/src/api/AVStockAPI.tsx
+++ frontend/src/api/AVStockAPI.tsx
@@ -22,28 +22,28 @@ export default {
 			'symbol': ticker.toUpperCase()
 		},
 		transformResponse: [function (data) {
-			// Do whatever you want to transform the data
+			// Делайте все, что вы хотите, чтобы преобразовать данные
 			console.log('Transforming data...')
-	
+
 			const json = JSON.parse(data)
-	
+
 			const dates = Object.keys(json['Time Series (Daily)']).reverse()
-	
-			// Construct response data for chart input
+
+			// Создайте ответные данные для ввода диаграммы
 			const closePrices = dates.map(date => date = {
 				date,
 				close: Number(json['Time Series (Daily)'][date]['4. close'])
 			})
-	
+
 			const symbol = json['Meta Data']['2. Symbol']
 			const refreshed = json['Meta Data']['3. Last Refreshed']
-	
+
 			data = {
 				symbol,
 				refreshed,
 				closePrices
 			}
-	
+
 			return data;
 		}],
 	}),
