e8c5ccdb20c83cf837d793dbaae4581754333acd
David2261 <bulatnasirov2003@gmail.com>
2023-08-09 19:41:15 +0300

    Add block last articles in main page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,53 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 
 
 const centerContent = `flex justify-center`;
+
+
+const DATA = [
+	{
+		id: 1,
+		title: "Philip Morris (PM): отчет за 2 кв. 2023 г. Лучший в своем роде"
+	},
+	{
+		id: 2,
+		title: "Ленэнерго: отчет за 2 кв. 2023 по РСБУ. Рекордные дивиденды зреют"
+	},
+	{
+		id: 3,
+		title: "РусАгро: отчет за 2 кв. 2023 по МСФО. Скоро укрупнение в показателях"
+	},
+	{
+		id: 4,
+		title: "Apple (AAPl): отчет за 3 кв. ф.г. Мечты яблочников об автомобиле"
+	},
+	{
+		id: 5,
+		title: "Newmont (NEM): отчет за 2 кв. 2023 г. Что не так с золотодобытчиками?"
+	},
+	{
+		id: 6,
+		title: "Ростелеком: отчет за 2 кв. 2023 по МСФО. Раскрытие вернулось!"
+	},
+	{
+		id: 7,
+		title: "Сбербанк: отчет за 2 кв. 2023 по МСФО. Лидер в форме!"
+	},
+	{
+		id: 8,
+		title: "Pfizer, Merck, Exxon Mobile, новый китайский портфель активов"
+	}
+];
+
+const contentList = DATA.map(value => 
+	// return <div><ContentPost title={value.title} /></div>;
+	<Fragment key={value.id}>
+		<div className="ml-10"><p className="text-xl hover:text-slate-700">{value.title}</p></div>
+	</Fragment>
+);
+
 export default class Home extends Component {
+
 	render() {
 		return (
 			<div className="bg-white flex flex-col pb-4 py-2 md:py-4 w-full">
@@ -21,18 +66,27 @@ export default class Home extends Component {
 				<div className="columns-3 mx-6 mb-10">
 					<div className="ml-4 mr-2">
 						<div className={`${centerContent}`}><img src="http://dummyimage.com/128x128/4d494d/686a82.jepg&text=placeholder+image" alt="placeholder+image" /></div>
-						<div className={`${centerContent}`}><p className="text-lg font-bold">Реальные и модельные портфели</p></div>
+						<div className={`${centerContent}`}><p className="text-lg font-bold hover:text-slate-700">Реальные и модельные портфели</p></div>
 						<div><p className="font-sans text-xl">Не знаете что покупать в свой портфель? Посмотрите на наши инвестиции</p></div>
 					</div>
 					<div className="mx-2">
 						<div className={`${centerContent}`}><img src="http://dummyimage.com/128x128/4d494d/686a82.jepg&text=placeholder+image" alt="placeholder+image" /></div>
-						<div className={`${centerContent}`}><p className="text-lg font-bold">Текущая ситуация</p></div>
+						<div className={`${centerContent}`}><p className="text-lg font-bold hover:text-slate-700">Текущая ситуация</p></div>
 						<div><p className="font-sans text-xl">Еженедельные обзоры главных тенденций на российских и глобальных финансовых площадках</p></div>
 					</div>
 					<div className="mr-4 ml-2">
 						<div className={`${centerContent}`}><img src="http://dummyimage.com/128x128/4d494d/686a82.jepg&text=placeholder+image" alt="placeholder+image" /></div>
-						<div className={`${centerContent}`}><p className="text-lg font-bold gap-4">Прокачайте себя</p></div>
+						<div className={`${centerContent}`}><p className="text-lg font-bold hover:text-slate-700">Прокачайте себя</p></div>
 						<div><p className="font-sans text-xl">Инвестируйте сначала в себя, а потом уже в другие активы. Сделайте Upgrade своих навыков</p></div>
+					</div>
+				</div>
+				<div className="m-0 bg-stone-300 w-full mb-10">
+					<div className="w-full grid grid-cols-2 gap-4 mt-4 mb-4">
+						<div className={`col-span-2 ${centerContent} my-4`}><h1 className="uppercase font-bold text-2xl">ПОСЛЕДНИЕ ОБЗОРЫ И СТАТЬИ</h1></div>
+						{contentList}
+						<div className={`col-span-2 ${centerContent} my-4`}>
+							<button className="bg-green-600 text-white p-3 rounded-md hover:bg-slate-500">ВСЕ ПОСЛЕДНИЕ МАТЕРИАЛЫ САЙТА</button>
+						</div>
 					</div>
 				</div>
 			</div>
