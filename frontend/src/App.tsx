38201920bbc7ed195083fcbecc4b4496736f9bf3
David2261 <bulatnasirov2003@gmail.com>
2023-08-14 17:51:00 +0300

    Completed Agreement and Emailagreement pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,8 @@ import Contact from './pages/static/Contact.tsx';
 import Responsibility from './pages/static/Responsibility.tsx';
 import Payanddelivery from './pages/static/Payanddelivery.tsx';
 import Confidentiality from './pages/static/Confidentiality.tsx';
+import Agreement from './pages/static/Agreement.tsx';
+import Emailagreement from './pages/static/Emailagreement.tsx';
 
 
 function App() {
@@ -21,14 +23,15 @@ function App() {
       <Navbar />
       <Routes >
         <Route path="/" exact element={ <Home /> } />
+        {/* Static pages */}
         <Route path="/about" element={ <About /> } />
         <Route path="/contact" element={ <Contact /> } />
         <Route path="/responsibility" element={ <Responsibility /> } />
         <Route path="/payanddelivery" element={ <Payanddelivery /> } />
         <Route path="/confidentiality" element={ <Confidentiality /> } />
-{/*        <Route path="/login" element={ <Login /> } />
-        <Route path="/emailagreement" element={ <Emailagreement /> } />
         <Route path="/agreement" element={ <Agreement /> } />
+        <Route path="/emailagreement" element={ <Emailagreement /> } />
+{/*        <Route path="/login" element={ <Login /> } />
         <Route path="/admin/login" element={ <LoginAdmin /> } />
         <Route path="/admin/register" element={ <RegisterAdmin /> } />
         */}
