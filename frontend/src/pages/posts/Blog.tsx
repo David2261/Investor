e101344979e84bc753d574bc53ce76b9c70a1bae
David2261 <bulatnasirov2003@gmail.com>
2023-08-16 22:49:34 +0300

    Add page blog

---
+++ frontend/src/pages/posts/Blog.tsx
@@ -0,0 +1,14 @@
+
+
+
+const Blog = () => {
+	return <div className="w-full h-auto">
+		<div className="flex flex-row px-10 py-10">
+			<div className="basis-1/3 px-4 flex flex-col"></div>
+			<div className="basis-2/3 flex flex-col"></div>
+		</div>
+	</div>
+};
+
+
+export default Blog;
