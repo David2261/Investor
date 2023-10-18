0d900a28cdf3ce19e58206c223f6246ddc171051
David2261 <bulatnasirov2003@gmail.com>
2023-10-18 23:35:15 +0300

    Problem with ES

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -2,19 +2,15 @@
 module.exports = {
   // среда тестирования - браузер
   testEnvironment: 'jest-environment-jsdom',
+  extensionsToTreatAsEsm: [".ts"],
   preset: 'ts-jest',
-  testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': 'identity-obj-proxy',
-  },
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': './node_modules/babel-jest',
-    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileTransformer.cjs',
+    '^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true }]
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
-
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '\\.(webp)$': '<rootDir>/src/__mocks__/fileMock.js',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  }
 }
