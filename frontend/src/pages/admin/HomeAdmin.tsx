96f755e4d6c2a26126292b6da87d6f1f3a9217a1
Bulat <65621634+David2261@users.noreply.github.com>
2023-12-28 00:11:53 +0300

    Add types and added chart for HomeAdmin

--- frontend/src/pages/admin/HomeAdmin.tsx
+++ frontend/src/pages/admin/HomeAdmin.tsx
@@ -1,20 +1,44 @@
 import { Fragment } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+// Style
 import '/src/styles/AdminHome.css';
+
+// Example Data
 import SideBarDATA from '/src/alpha_test_data/admin_data_groups.tsx';
 import AdminDATARecent from '/src/alpha_test_data/admin_data_recent.tsx';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const SideBar = SideBarDATA.map((value: {
     id: number,
     title: string,
-    content: [],
+    content: string[],
 }) => 
     <Fragment key={value.id}>
         <div className='home-bar-content'>
             <h1>{value.title}</h1>
-            {value.content.map(function(item) {
+            {value.content.map((item) => {
                 return <p>{item}</p>
             })}
-            {/* {value.content.length > 0 ? <p>{value.content[0]}</p> : null} */}
         </div>
     </Fragment>
 );
@@ -28,6 +52,45 @@ const ContentRecentChanges = AdminDATARecent.map((value: {
     </Fragment>
 );
 
+const lineOption = {
+    responsive: true,
+    plugins: {
+        legend: {
+          position: 'top' as const,
+        },
+        title: {
+          display: true,
+          text: 'Просмотры за месяцы',
+        },
+    },
+}
+
+const lineData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
+
 const HomeAdmin = () => {
     return <>
     <div className='home-body'>
@@ -37,7 +100,7 @@ const HomeAdmin = () => {
         </div>
         <div className='home-content'>
             <div className='content-chart'>
-                <h1>BLOCK CHART</h1>
+                <Line options={lineOption} data={lineData} />
             </div>
             <div className='content-recent-changes'>
                 <h1>Последние действия</h1>
