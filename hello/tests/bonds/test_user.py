4dfd9e0f85d2c6e72783868ac20832f5ece9616b
Bulat <65621634+David2261@users.noreply.github.com>
2024-01-23 17:37:34 +0300

    Deleted ckeditor and added test for api

---
+++ hello/tests/bonds/test_user.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework.test import APIClient
+
+client = APIClient()
+
+@pytest.mark.django_db
+def test_register_user():
+    f_user = dict(
+        username="Michael",
+        email="michael@gmail.com",
+        is_staff = False,
+        is_active=True,
+    )
+
+    response = client.post("api/auth/registration", f_user)
+    data = response.data
+    assert data["username"] == f_user["username"]
