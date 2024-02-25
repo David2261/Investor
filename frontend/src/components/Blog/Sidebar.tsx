7d5c4173974c6af17af3053c30e91dfcbef19003
Bulat <65621634+David2261@users.noreply.github.com>
2024-02-25 20:51:26 +0300

    Solved category sidebar

--- frontend/src/components/Blog/Sidebar.tsx
+++ frontend/src/components/Blog/Sidebar.tsx
@@ -1,42 +1,25 @@
 import { Key, Fragment, FunctionComponent } from "react";
 import "../../styles/components/Sidebar.css";
-import CategoryName from "./CategoryName";
 
 interface SidebarPropsType {
-	isNavOpen: boolean,
-	data: {
-		id: Key,
-		category: number,
-		img: string,
-	}[],
+	data: PropsType[],
 }
 
 type PropsType = {
 	id: Key,
-	category: number,
-	img: string | undefined,
+	name: string,
+	slug: string
 }
 
 const Sidebar: FunctionComponent<SidebarPropsType> = (props: SidebarPropsType) => {
-	// return console.log(props.data[0])
-	return (props.data.map((value: PropsType) => 
-		<Fragment key={value.id}>
-			<li className="px-4 py-4 flex" title="home">
-				<a href={`#${value.category}`} className="flex flex-row">
-					<div className="icon-wrapper px-2">
-						<span className="material-symbols-outlined">
-							{value.img ?
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-shop" viewBox="0 0 16 16">
-								<path d={value.img} />
-							</svg>
-							: value.category
-							}
-						</span>
-					</div>
-					{props.isNavOpen ? <span className="item-text"><CategoryName id={value.category} /></span> : false}
-				</a>
-			</li>
-		</Fragment>
+	return (props.data.map((value: PropsType) =>
+	<Fragment key={value.id}>
+		<li className="px-4 py-4 flex" title="home">
+			<a href={`#${value.slug}`} className="flex flex-row">
+			{value.name}
+			</a>
+		</li>
+	</Fragment>
 	));
 }
 
--- frontend/src/pages/posts/Blog.tsx
+++ frontend/src/pages/posts/Blog.tsx
@@ -1,4 +1,4 @@
-import { Component, FunctionComponent, Key, useState } from 'react';
+import { Component, Key } from 'react';
 import axios from 'axios';
 import '../../styles/Blog.css';
 import PostsList from '../../components/Blog/PostsList';
@@ -9,14 +9,13 @@ import Sidebar from '../../components/Blog/Sidebar';
 
 
 interface BlogAPIType {
-	isNavOpen: boolean,
 	data: {
 		id: Key,
 		category: number,
-		img: string,
+		title: string,
+		img: string | undefined,
 	}[]
 }
-
 
 interface State {
 	data: [];
@@ -24,41 +23,12 @@ interface State {
 	placeholder: string;
 }
 
-const DataTab: FunctionComponent<{ data: BlogAPIType["data"] }> = (data) =>  {
-	const [isNavOpen, setIsNavOpen] = useState(false)
-	const changeControllerIconStyle = (isNavOpen: boolean): React.CSSProperties => {
-		return {
-			color: isNavOpen ? 'red' : 'blue',
-		};
-	}
-
-	function toggleNavigationBarState() {
-		setIsNavOpen(!isNavOpen);
-	}
-
-	return (
-		<nav className={`fixed pt-4 pl-4 z-20 ${isNavOpen ? 'open' : ''}`}>
-			<ul className="transition transform duration-500 text-2xl">
-				<button className="px-4" style={changeControllerIconStyle(isNavOpen)} onClick={toggleNavigationBarState} title="open navigation bar">
-					{isNavOpen ?
-					<span className="material-symbols-outlined" style={changeControllerIconStyle(isNavOpen)}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
-							<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-							<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-						</svg>
-					</span>
-					:
-					<span className="material-symbols-outlined" style={changeControllerIconStyle(isNavOpen)}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-							<path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-						</svg>
-					</span>}
-				</button>
-				{ data ? <Sidebar data={data} isNavOpen={isNavOpen} /> : false }
-			</ul>
-		</nav>
-	)
-
+interface CategoryAPIType {
+	data: {
+		id: Key,
+		name: string,
+		slug: string
+	}[]
 }
 
 class Blog extends Component<{}, State> {
@@ -94,7 +64,7 @@ class Blog extends Component<{}, State> {
 	render() {
 	return <>
 		<h1 className='blog-header'>NEWS</h1>
-		<DataTab data={this.state.data} />		
+		<DataTab />
 		<div className='blog-content'>
 			<PostsList data={this.state.data} />
 		</div>
@@ -102,5 +72,47 @@ class Blog extends Component<{}, State> {
 	}
 }
 
+// Боковая панель навигации по категориям
+class DataTab extends Component<{}, State> {
+	constructor(props: CategoryAPIType) {
+		super(props);
+		this.state = {
+			data: [],
+			loaded: false,
+			placeholder: "Loading",
+		};
+	}
+
+	async componentDidMount() {
+		await axios.get('http://127.0.0.1:8000/api/articles/category/all/')
+		.then(response => {
+			if (response.status > 400) {
+				return this.setState(() => {
+					return { placeholder: "Something went wrong!" };
+				});
+			}
+			return (response.data);
+		})
+		.then(data => {
+			this.setState(() => {
+				return {
+					data,
+					loaded: true
+				};
+			});
+		});
+	}
+
+	render() {
+		console.log(this.state.data.map((value) => value))
+		return (
+		<nav className="fixed pt-4 pl-4 z-20">
+			<ul className="transition transform duration-500 text-xl">
+				{ this.state.data ? <Sidebar data={this.state.data} /> : false }
+			</ul>
+		</nav>
+		);
+	}
+}
 
 export default Blog;
