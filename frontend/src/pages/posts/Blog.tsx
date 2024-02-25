5d9322fdd31c82bae486171395b0cb3012d36f82
Bulat <65621634+David2261@users.noreply.github.com>
2024-02-25 21:08:22 +0300

    Mapped DatatTab

--- frontend/src/pages/posts/Blog.tsx
+++ frontend/src/pages/posts/Blog.tsx
@@ -2,7 +2,7 @@ import { Component, Key } from 'react';
 import axios from 'axios';
 import '../../styles/Blog.css';
 import PostsList from '../../components/Blog/PostsList';
-import Sidebar from '../../components/Blog/Sidebar';
+import DataTab from '../../components/Blog/DataTab';
 // Example data
 // import DATAPOSTS from "../../alpha_test_data/blog_data_posts.json";
 // import DATA from '../../alpha_test_data/blog_data_categories.json';
@@ -21,14 +21,6 @@ interface State {
 	data: [];
 	loaded: boolean;
 	placeholder: string;
-}
-
-interface CategoryAPIType {
-	data: {
-		id: Key,
-		name: string,
-		slug: string
-	}[]
 }
 
 class Blog extends Component<{}, State> {
@@ -69,49 +61,6 @@ class Blog extends Component<{}, State> {
 			<PostsList data={this.state.data} />
 		</div>
 	</>
-	}
-}
-
-// Боковая панель навигации по категориям
-class DataTab extends Component<{}, State> {
-	constructor(props: CategoryAPIType) {
-		super(props);
-		this.state = {
-			data: [],
-			loaded: false,
-			placeholder: "Loading",
-		};
-	}
-
-	async componentDidMount() {
-		await axios.get('http://127.0.0.1:8000/api/articles/category/all/')
-		.then(response => {
-			if (response.status > 400) {
-				return this.setState(() => {
-					return { placeholder: "Something went wrong!" };
-				});
-			}
-			return (response.data);
-		})
-		.then(data => {
-			this.setState(() => {
-				return {
-					data,
-					loaded: true
-				};
-			});
-		});
-	}
-
-	render() {
-		console.log(this.state.data.map((value) => value))
-		return (
-		<nav className="fixed pt-4 pl-4 z-20">
-			<ul className="transition transform duration-500 text-xl">
-				{ this.state.data ? <Sidebar data={this.state.data} /> : false }
-			</ul>
-		</nav>
-		);
 	}
 }
 
