1d2627ee2afd57a79eeade33ece9702ca74c5a16
Bulat <65621634+David2261@users.noreply.github.com>
2024-02-26 21:29:46 +0300

    Added home icon for categories sidebar

--- frontend/src/components/Blog/DataTab.tsx
+++ frontend/src/components/Blog/DataTab.tsx
@@ -47,10 +47,19 @@ class DataTab extends Component<{}, State> {
 		});
 	}
 
+
 	render() {
-		console.log(this.state.data.map((value) => value))
 		return (
 		<nav className="fixed pt-4 pl-4 z-20">
+			{
+				this.state.data ? (
+					<span className="text-sm">
+						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 24 24">
+							<path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path>
+						</svg>
+					</span>)
+					: false
+			}
 			<ul className="transition transform duration-500 text-xl">
 				{ this.state.data ? <Sidebar data={this.state.data} /> : false }
 			</ul>
