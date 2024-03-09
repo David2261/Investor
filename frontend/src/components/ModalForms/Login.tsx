b8b7890e97ab27c5c5bc6640deee738d34a34719
Bulat <65621634+David2261@users.noreply.github.com>
2024-03-09 13:56:52 +0300

    Added modal form for login

--- frontend/src/components/ModalForms/Login.tsx
+++ frontend/src/components/ModalForms/Login.tsx
@@ -7,26 +7,22 @@ const Login = (props) => {
     <div className="fixed z-10 w-full h-full backdrop-blur-sm bg-white/30 h-12">
 			<div className='screen'>
 				<div className="screen-1">
-					<img className='logo' alt="logo" src={IH} />
+				<img className='logo' alt="logo" src={IH} />
 					<button onClick={props.setIsOpen} className="fixed top-16 right-8">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-white w-6 h-6">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-white w-32 h-32">
 							<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
 						</svg>
 					</button>
-
 					<div className="email">
 						<label htmlFor="email">Email Address</label>
 						<div className="sec-2">
-							<ion-icon name="mail-outline"></ion-icon>
-							<input type="email" name="email" placeholder="Username@gmail.com"/>
+							<input type="email" name="email" placeholder="Example@gmail.com"/>
 						</div>
 					</div>
 					<div className="password">
 						<label htmlFor="password">Password</label>
 						<div className="sec-2">
-							<ion-icon name="lock-closed-outline"></ion-icon>
 							<input className="pas" type="password" name="password" placeholder="············"/>
-							<ion-icon className="show-hide" name="eye-outline"></ion-icon>
 						</div>
 					</div>
 					<button className="signup">Login</button>
