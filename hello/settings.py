27969be20b8186d11089ba8cc74a6623a5d26943
Bulat <65621634+David2261@users.noreply.github.com>
2024-03-27 22:56:56 +0300

    Setup connect PG Database with django

--- hello/settings.py
+++ hello/settings.py
@@ -136,18 +136,18 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	}
 	# 'default': {
-	#     'ENGINE': 'django.db.backends.postgresql',
-	#     'NAME': 'postgres',
-	#     'USER': env('USER'),
-	#     'PASSWORD': env('PASSWORD_SQL'),
-	#     'HOST': '127.0.0.1',
-	#     'PORT': 5432
+	# 	'ENGINE': 'django.db.backends.sqlite3',
+	# 	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	# }
+	'default': {
+	    'ENGINE': 'django.db.backends.postgresql',
+	    'NAME': env('PGDB'),
+	    'USER': env('PGUSER'),
+	    'PASSWORD': env('PGPASSWORD'),
+	    'HOST': env('PGHOST'),
+	    'PORT': env('PGPORT'),
+	}
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
