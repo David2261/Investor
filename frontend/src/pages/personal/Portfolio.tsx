21a9e3c28e58b12e14a7aa5cb1f075cdf9195a3c
Bulat <65621634+David2261@users.noreply.github.com>
2024-04-03 23:57:00 +0300

    Updated portfolio page

--- frontend/src/pages/personal/Portfolio.tsx
+++ frontend/src/pages/personal/Portfolio.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Portfolio = () => {
     return <>
-    <h1>PORTFOLIO</h1>
+        <div className="w-full h-full">
+            <div className="flex w-full px-24 mt-10 mb-10">
+                <div className='w-full flex justify-center border rounded-lg bg-slate-100 pt-10 pb-10 px-24'>
+                    <div className='flex flex-row mb-10'>
+                        <h1 className="font-bold text-3xl px-4">Аккаунт</h1>
+                        <p className='uppercase text-lg font-light bg-cyan-100 rounded-lg'>free</p>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     </>
 }
 
