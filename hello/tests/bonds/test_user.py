818e527bcda1068be6c19b325b0d8da45eec2af5
Bulat <65621634+David2261@users.noreply.github.com>
2024-04-30 01:42:14 +0300

    Update test_user.py

--- hello/tests/bonds/test_user.py
+++ hello/tests/bonds/test_user.py
@@ -10,33 +10,27 @@ client = APIClient()
 
 @pytest.mark.django_db
 def test_registration():
-    # Create a new user
-    User = get_user_model()
-    user = User.objects.create_user(
-        username='testuser',
-        email='testuser@example.com',
-        password='testpassword',
-    )
+	# Create a new user
+	User = get_user_model()
+	user = User.objects.create_user(
+		username='testuser',
+		email='testuser@example.com',
+		password='testpassword',
+	)
 
-    # Send an HTTP POST request to the registration endpoint
-    data = {
-        'username': 'newuser',
-        'email': 'newuser@example.com',
-        'password': 'newpassword',
-        'password2': 'newpassword',
-    }
-    response = client.post(reverse('authentication:user_registration'), data=data)
-
-    # Check the response status code
-    assert response.status_code == status.HTTP_201_CREATED
-
-    # Check the response data
-    response_data = json.loads(response.content)
-    assert response_data['token'] is not None
-
-    # Check that a new user was created
-    new_user = User.objects.get(username='newuser')
-    assert new_user.email == 'newuser@example.com'
+	# Send an HTTP POST request to the registration endpoint
+	data = {
+		'username': 'newuser',
+		'email': 'newuser@example.com',
+		'password': 'newpassword',
+		'password2': 'newpassword',
+	}
+	response = client.post(reverse('authentication:user_registration'), data=data)
+	assert response.status_code == status.HTTP_201_CREATED
+	response_data = json.loads(response.content)
+	assert response_data['token'] is not None
+	new_user = User.objects.get(username='newuser')
+	assert new_user.email == 'newuser@example.com'
 
 
 def test_login_user():
