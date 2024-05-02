d3ca92b431a129d22bbb861f6304601eee1554a1
Bulat <65621634+David2261@users.noreply.github.com>
2024-05-02 22:02:12 +0300

    Added registrationserializer

--- hello/apps/authentication/serializers.py
+++ hello/apps/authentication/serializers.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from django.contrib.auth import authenticate
-from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
 
 from .models import User
 
@@ -13,32 +14,51 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 		# Add custom claims
 		token['username'] = user.username
-		# ...
 
 		return token
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-	password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+	email = serializers.EmailField(
+			required=True,
+			validators=[UniqueValidator(queryset=User.objects.all())]
+			)
+
+	password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+	password2 = serializers.CharField(write_only=True, required=True)
 
 	class Meta:
-		model = get_user_model()
-		fields = ('username', 'email', 'password', 'password2')
-		extra_kwargs = {
-			'password': {'write_only': True},
-		}
+		model = User
+		fields = ('username', 'password', 'password2', 'email')
 
 	def validate(self, attrs):
 		if attrs['password'] != attrs['password2']:
-			raise serializers.ValidationError({'password': 'Passwords must match.'})
+			raise serializers.ValidationError(
+				{"password": "Password fields didn't match."}
+			)
+		
+		if attrs['email'] is None:
+			raise serializers.ValidationError(
+				'An email address is required to log in.'
+			)
+
+		if attrs['password'] is None:
+			raise serializers.ValidationError(
+				'A password is required to log in.'
+			)
+
 		return attrs
 
 	def create(self, validated_data):
-		user = get_user_model().objects.create_user(
+		user = User.objects.create(
 			username=validated_data['username'],
-			email=validated_data['email'],
-			password=validated_data['password'],
+			email=validated_data['email']
 		)
+
+		
+		user.set_password(validated_data['password'])
+		user.save()
+
 		return user
 
 
--- hello/apps/authentication/views.py
+++ hello/apps/authentication/views.py
@@ -4,28 +4,18 @@ from rest_framework import status
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from .serializers import RegistrationSerializer
 from .serializers import UserSerializer
+from .models import User
 
 
-class RegistrationAPIView(APIView):
+class RegistrationAPIView(CreateAPIView):
+	queryset = User.objects.all()
 	permission_classes = [permissions.AllowAny]
 	serializer_class = RegistrationSerializer
-
-	def post(self, request):
-		serializer = self.serializer_class(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		serializer.save()
-
-		return Response(
-			{
-				'token': serializer.data.get('token', None),
-			},
-			status=status.HTTP_201_CREATED,
-		)
 
 
 class CustomTokenRefreshView(TokenRefreshView):
