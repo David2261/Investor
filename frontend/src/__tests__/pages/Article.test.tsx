f2dd3902810a0abe01fe170acecf63fe1b9c611b
Bulat <65621634+David2261@users.noreply.github.com>
2024-05-05 23:33:15 +0300

    Updated test

--- frontend/src/__tests__/pages/Article.test.tsx
+++ frontend/src/__tests__/pages/Article.test.tsx
@@ -8,30 +8,33 @@ import Article from '../../pages/posts/Article';
 
 
 describe('Article page', () => {
-    let container:any = null;
+	let container:any = null;
 
 	beforeEach(() => {
-        container = document.createElement("div");
-        document.body.appendChild(container);    
-    });
-
-    afterEach(() => {
-        unmountComponentAtNode(container);
-        container.remove();
-        container = null;
-    });
-
-	test('Found img in document Article', () => {
-        act(() => {
-            render(<Article />, container);
-        });
-		expect(container.querySelector('img'));
+		container = document.createElement("div");
+		document.body.appendChild(container);    
 	});
 
-    test('Found logo img in document Article', () => {
-        act(() => {
-            render(<Article />);
-        });
+	afterEach(() => {
+		unmountComponentAtNode(container);
+		container.remove();
+		container = null;
+	});
+
+	it('renders an image', async () => {
+		const container = document.createElement('div');
+	
+	await act(async () => {
+		render(<Article />, { container });
+		});
+	
+		expect(container.querySelector('img')).not.toBeNull();
+	});
+
+	test('Found logo img in document Article', () => {
+		act(() => {
+			render(<Article />);
+		});
 		expect(screen.getByRole('img', {name: 'logo'}));
 	});
 
