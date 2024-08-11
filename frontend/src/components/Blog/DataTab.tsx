5e3fb840f863b57a2589520626e3e0c21e891a5d
Bulat <65621634+David2261@users.noreply.github.com>
2024-08-11 22:45:50 +0300

    Added menu and template for news page

--- frontend/src/components/Blog/DataTab.tsx
+++ frontend/src/components/Blog/DataTab.tsx
@@ -1,71 +1,30 @@
-import axios from "axios";
-import { Key, PureComponent } from "react";
-import Sidebar from "./Sidebar";
+import { useState } from "react";
+// import Sidebar from "./Sidebar";
+// Styles
+import '../../styles/components/Blog/DataTabStyles.css';
+// Assets
+import homeBlack from '../../assets/icons/menu_black.svg';
+import sortBlack from '../../assets/icons/sort_black.svg';
 
-interface State {
-	data: [];
-	loaded: boolean;
-	placeholder: string;
-}
 
-interface CategoryAPIType {
-	data: {
-		id: Key,
-		name: string,
-		slug: string
-	}[]
-}
 
 // Боковая панель навигации по категориям
-class DataTab extends PureComponent<{}, State> {
-	constructor(props: CategoryAPIType) {
-		super(props);
-		this.state = {
-			data: [],
-			loaded: false,
-			placeholder: "Loading",
-		};
+const DataTab = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	function openModal() {
+		setIsOpen(true);
 	}
-
-	async componentDidMount() {
-		await axios.get('http://127.0.0.1:8000/api/articles/category/all/')
-		.then(response => {
-			if (response.status > 400) {
-				return this.setState(() => {
-					return { placeholder: "Something went wrong!" };
-				});
-			}
-			return (response.data);
-		})
-		.then(data => {
-			this.setState(() => {
-				return {
-					data,
-					loaded: true
-				};
-			});
-		});
+	function closeModal() {
+		setIsOpen(false);
 	}
-
-
-	render() {
-		return (
-		<nav className="fixed pt-4 pl-4 z-20">
-			{
-				this.state.data.length != 0 ? (
-					<span className="text-sm">
-						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 24 24">
-							<path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path>
-						</svg>
-					</span>)
-					: false
-			}
-			<ul className="transition transform duration-500 text-xl">
-				{ this.state.data ? <Sidebar data={this.state.data} /> : false }
-			</ul>
-		</nav>
-		);
-	}
+	return <>
+	{!isOpen ?
+		<div className="flex justify-between">
+			<button className="data-tab-sidebar-close-btn"><img src={homeBlack} /> Категории</button>
+			<button className="data-tab-sidebar-right-btn"><img src={sortBlack} />Сортировка</button>
+		</div>
+	: false}
+	</>
 }
 
 export default DataTab;
--- frontend/src/components/Blog/PostsList.tsx
+++ frontend/src/components/Blog/PostsList.tsx
@@ -11,6 +11,8 @@ interface PostsListType {
 		title: string,
 		img: string | undefined,
 		slug: string,
+		time_create: string,
+		description: string
 	}[],
 }
 
@@ -24,19 +26,27 @@ type PropsType = {
 		title: string,
 		img: string | undefined,
 		slug: string,
+		time_create: string,
+		description: string
 	},
 }
 
 // Блок статьи
 const PostsList: FunctionComponent<PostsListType> = (props: PostsListType) => {
 	return (props.data.map((value: PropsType["args"]) =>
-		<Link key={value.id} to={`/news/${value.category.slug}/${value.slug}`}>
-			<div className="blog-content-post">
-				<img src={value.img} alt="" />
-				<p>{value.title}</p>
-				<p className="uppercase">{value.category.name}</p>
-			</div>
-		</Link>
+		<div className="border-b-2 py-4 border-slate-200 mb-4">
+			<Link key={value.id} to={`/news/${value.category.slug}/${value.slug}`}>
+				<div className="grid grid-cols-3 gap-8">
+					<div className="col-span-2">
+						<p className="uppercase font-bold pb-2">{value.title}</p>
+						<p className="text-base">{value.description.slice(0, 100)}...</p>
+					</div>
+					<div>
+						<img src={value.img} className="rounded-lg" alt="" />
+					</div>
+				</div>
+			</Link>
+		</div>
 	));
 }
 
