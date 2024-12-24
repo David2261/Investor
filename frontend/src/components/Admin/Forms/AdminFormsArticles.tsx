a9b633e0c651c7bcce5b35d14e0683e623eced8d
Bulat <65621634+David2261@users.noreply.github.com>
2024-12-24 13:41:20 +0300

    Updated packege added ckeditor for articles

--- frontend/src/components/Admin/Forms/AdminFormsArticles.tsx
+++ frontend/src/components/Admin/Forms/AdminFormsArticles.tsx
@@ -1,4 +1,198 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+import translations from 'ckeditor5/translations/ru.js';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import 'ckeditor5/ckeditor5.css';
+import '../../../styles/components/Admin/AdminFormsArticles.css';
+import {
+    ClassicEditor,
+    AutoLink,
+    Autosave,
+    BalloonToolbar,
+    BlockQuote,
+    Bold,
+    Bookmark,
+    Code,
+    CodeBlock,
+    Essentials,
+    Heading,
+    Highlight,
+    HorizontalLine,
+    HtmlEmbed,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    Paragraph,
+    SpecialCharacters,
+    Strikethrough,
+    Table,
+    TableCellProperties,
+    TableProperties,
+    TableToolbar,
+    Underline
+} from 'ckeditor5';
+
+
+const LICENSE_KEY =
+    'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3MzYyOTQzOTksImp0aSI6ImVjM2EwOGRlLWFiNGMtNDExYi05MDRhLTBiNDU4Yzc2YWIxYSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjVjZDdmZWQzIn0.ydy-DtATOrDKfQDS_S9GXlBQ-9sagVNtQLKwdhlBeiZl9_ohZ3KzxPmSyr_Dpx_OgeUt0WLE4ExOu3qlYpjziQ';
+
+
+function Description() {
+    const editorContainerRef = useRef(null);
+    const editorRef = useRef(null);
+    const [isLayoutReady, setIsLayoutReady] = useState(false);
+
+    useEffect(() => {
+        setIsLayoutReady(true);
+
+        return () => setIsLayoutReady(false);
+    }, []);
+
+    const { editorConfig } = useMemo(() => {
+        if (!isLayoutReady) {
+            return {};
+        }
+
+        return {
+            editorConfig: {
+                toolbar: {
+                    items: [
+                        'heading',
+                        '|',
+                        'bold',
+                        'italic',
+                        'underline',
+                        'strikethrough',
+                        'code',
+                        '|',
+                        'specialCharacters',
+                        'horizontalLine',
+                        'link',
+                        'bookmark',
+                        'insertTable',
+                        'highlight',
+                        'blockQuote',
+                        'codeBlock',
+                        'htmlEmbed',
+                        '|',
+                        'outdent',
+                        'indent'
+                    ],
+                    shouldNotGroupWhenFull: false
+                },
+                plugins: [
+                    AutoLink,
+                    Autosave,
+                    BalloonToolbar,
+                    BlockQuote,
+                    Bold,
+                    Bookmark,
+                    Code,
+                    CodeBlock,
+                    Essentials,
+                    Heading,
+                    Highlight,
+                    HorizontalLine,
+                    HtmlEmbed,
+                    Indent,
+                    IndentBlock,
+                    Italic,
+                    Link,
+                    Paragraph,
+                    SpecialCharacters,
+                    Strikethrough,
+                    Table,
+                    TableCellProperties,
+                    TableProperties,
+                    TableToolbar,
+                    Underline
+                ],
+                balloonToolbar: ['bold', 'italic', '|', 'link'],
+                heading: {
+                    options: [
+                        {
+                            model: 'paragraph',
+                            title: 'Paragraph',
+                            class: 'ck-heading_paragraph'
+                        },
+                        {
+                            model: 'heading1',
+                            view: 'h1',
+                            title: 'Heading 1',
+                            class: 'ck-heading_heading1'
+                        },
+                        {
+                            model: 'heading2',
+                            view: 'h2',
+                            title: 'Heading 2',
+                            class: 'ck-heading_heading2'
+                        },
+                        {
+                            model: 'heading3',
+                            view: 'h3',
+                            title: 'Heading 3',
+                            class: 'ck-heading_heading3'
+                        },
+                        {
+                            model: 'heading4',
+                            view: 'h4',
+                            title: 'Heading 4',
+                            class: 'ck-heading_heading4'
+                        },
+                        {
+                            model: 'heading5',
+                            view: 'h5',
+                            title: 'Heading 5',
+                            class: 'ck-heading_heading5'
+                        },
+                        {
+                            model: 'heading6',
+                            view: 'h6',
+                            title: 'Heading 6',
+                            class: 'ck-heading_heading6'
+                        }
+                    ]
+                },
+                initialData:
+                    '<h2>Напишите полезную и интересную статью! 🎉</h2>\n',
+                language: 'ru',
+                licenseKey: LICENSE_KEY,
+                link: {
+                    addTargetToExternalLinks: true,
+                    defaultProtocol: 'https://',
+                    decorators: {
+                        toggleDownloadable: {
+                            mode: 'manual',
+                            label: 'Downloadable',
+                            attributes: {
+                                download: 'file'
+                            }
+                        }
+                    }
+                },
+                menuBar: {
+                    isVisible: true
+                },
+                placeholder: 'Type or paste your content here!',
+                table: {
+                    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+                },
+                translations: [translations]
+            }
+        };
+    }, [isLayoutReady]);
+
+    return (
+        <div className="main-container">
+            <div className="editor-container editor-container_classic-editor" ref={editorContainerRef}>
+                <div className="editor-container__editor">
+                    <div ref={editorRef}>{editorConfig && <CKEditor editor={ClassicEditor} config={editorConfig} />}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 
 const AdminFormsArticles = () => {
     return (
@@ -19,11 +213,7 @@ const AdminFormsArticles = () => {
                 <div className="flex items-start gap-2">
                     <label className="w-40 text-right text-white pt-1" htmlFor="content">Текст статьи:</label>
                     <div className="flex-grow">
-                        <textarea
-                            id="content"
-                            rows={5}
-                            className="w-full border border-gray-400 rounded px-2 py-1 text-black"
-                        ></textarea>
+                        <Description />
                     </div>
                 </div>
 
@@ -56,10 +246,10 @@ const AdminFormsArticles = () => {
                     <label className="w-40 text-right text-white" htmlFor="publication">Публикация:</label>
                     <div className="flex-grow">
                         <input
-                            id="publication"
-                            type="checkbox"
-                            className="w-5 h-5 border border-gray-400 rounded"
-                        />
+							id="publication"
+							type="checkbox"
+							className="w-5 h-5 border border-gray-400 rounded"
+						/>
                     </div>
                 </div>
 
